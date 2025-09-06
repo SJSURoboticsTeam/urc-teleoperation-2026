2a240963de1ac272396904acd3b0f429d4572a0e
Stray-Rat <strayed.rat@gmail.com>
2025-09-06 11:15:10 -0700

    implemented basic serial communication, tested with an arduino that echoes back serial inputs

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
 import { Server } from "socket.io";
+import {SerialPort} from 'serialport'
+import { ByteLengthParser } from '@serialport/parser-byte-length'
+import { autoDetect } from '@serialport/bindings-cpp';
+
+
+const ID = {
+    STOP: 0x0E,
+    HEARTBEAT: 0x0D,
+    HEARTBEAT_REPLY: 0x0F,
+    HOMING_SEQUENCE: 0x110,
+    GET_OFFSET: 0x111,
+    RETURN_OFFSET: 0x112,
+    SET_TRANSLATION_VELOCITY: 0x113,
+    SET_ROTATIONAL_VELOCITY: 0x114,
+    SET_VELOCITY_RESPONSE: 0x115,
+    GET_ESTIMATED_VELOCITY: 0x116,
+    RETURN_ESTIMATED_VELOCITY: 0x117,
+    RETURN_ESTIMATED_ROTATION: 0x118,
+    CONFIG: 0x119
+}
+
+
 
 const port = 4000;
 const io = new Server({
@@ -18,3 +40,23 @@ io.on("connection", (socket) => {
 io.listen(port, () => {
     console.log(`Server: http://localhost:${port}`)
 })
+
+const binding = autoDetect();
+const devices = await binding.list();
+
+const rpiSerial = new SerialPort({
+    path: devices[0].path,
+    baudRate: 9600
+})
+
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
+const parser = rpiSerial.pipe(new ByteLengthParser({length: 1}))
+parser.on('data', (data) => {
+    console.log(decoder.decode(data));
+})
+
+setInterval(() => {
+    rpiSerial.write(encoder.encode("1"));
+}, 1000)
