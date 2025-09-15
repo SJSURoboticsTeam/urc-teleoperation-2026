24338f0eb7b2251a8047340d18ecff7ff99df1bf
Agrika <agrika.gupta@sjsu.edu>
2025-09-15 10:37:55 -0700

    modified:   package-lock.json 	modified:   src/panes/DriveView.jsx 	new file:   src/trial.jsx

--- src/panes/DriveView.jsx
+++ src/panes/DriveView.jsx
@@ -1,26 +1,62 @@
 import 'react-resizable/css/styles.css' // Import default styles
 import { createContext } from 'react';
 import { useContext } from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import { green } from "@mui/material/colors";
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 //test
 // In the future, it's one of these per view (drive, arm, science, etc)
 const ThemeContext = createContext('null');
 function DriveThemeContext(){
 
 }
+
 function DriveUi(){
     const [tolerance, setTolerance] = useState('0');
     const [sidewaysVelocity, setSidewaysVelocity] = useState('0');
     const [forwardsVelocity, setForwardVelocity] = useState('0');
     const [rotationalVelocity, setRotationalVelocity] = useState('0');
+    const gamepads={}
+    const [controllerno,setControllerno]=useState(0)
+    const gamepadHandler = (event, connected) => {
+        const gamepad = event.gamepad;
+        if (connected) {
+        gamepads[gamepad.index] = gamepad;
+        } else {
+        delete gamepads[gamepad.index];
+        alert("you disconnected controller index "+gamepad.index);
+        }
+    };
+    useEffect(()=>{
+        if (controllerno>0)
+        alert("Number of controllers connected:"+ controllerno);
+    },[controllerno])
+    useEffect(() => {
+        const handleConnect = (e) => {
+        setControllerno(prev => {return prev + 1;});
+        gamepadHandler(e, true);
+        };
+    const handleDisconnect = (e) => {
+        setControllerno(prev => {Math.max(prev - 1, 0)});
+        gamepadHandler(e, false);
+    };
 
+    window.addEventListener("gamepadconnected", handleConnect);
+    window.addEventListener("gamepaddisconnected", handleDisconnect);
+
+    return () => {
+      window.removeEventListener("gamepadconnected", handleConnect);
+      window.removeEventListener("gamepaddisconnected", handleDisconnect);
+    };
+    }, []);
     const handleChange = (event) => {
         setTolerance(event.target.value);
     };
     return (
+        <>
         <section>
             <div sx={{display: 'flex', justifyContent: "flex-start"}}>
                 <TextField
@@ -87,7 +123,22 @@ function DriveUi(){
                 </Typography>
             </Box>
             </Box>
+
+            <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '350px',
+                height: '200px',
+                marginTop: 5,
+                }}>
+                <SportsEsportsIcon sx={{color:controllerno>0?green[500]:"black", width:100,height:100}} id="gamepadicon"/> 
+                {/* sx={{color:pink[500]}} */}
+            </Box>
         </section>
+        </>
         );
     }
 
---
+++ src/trial.jsx
@@ -0,0 +1,30 @@
+const gamepads = {};
+
+function gamepadHandler(event, connected) {
+  const gamepad = event.gamepad;
+  // Note:
+  // gamepad === navigator.getGamepads()[gamepad.index]
+
+  if (connected) {
+    alert('connected');
+    gamepads[gamepad.index] = gamepad;
+  } else {
+    alert('disconnected');
+    delete gamepads[gamepad.index];
+  }
+}
+
+window.addEventListener(
+  "gamepadconnected",
+  (e) => {
+    gamepadHandler(e, true);
+  },
+  false,
+);
+window.addEventListener(
+  "gamepaddisconnected",
+  (e) => {
+    gamepadHandler(e, false);
+  },
+  false,
+);
