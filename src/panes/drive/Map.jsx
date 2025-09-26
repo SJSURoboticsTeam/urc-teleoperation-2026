cfe45ef43171eeffe879c2ab8e59a9fa8b99734a
Dom <dominic.dang@sjsu.edu>
2025-09-26 10:57:11 -0700

    Added MapContainer

--- src/panes/drive/Map.jsx
+++ src/panes/drive/Map.jsx
@@ -1,10 +1,17 @@
 import 'react-resizable/css/styles.css'
 import Box from '@mui/material/Box'
+//import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 export default function Map(){
+    const position = [51.505, -0.09]
     return(
         <Box>
-           map
+           {/* <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{
+                height: "100%",
+                width: "100%"
+            }}>
+            </MapContainer> */}
         </Box>
+        
     )
 }
