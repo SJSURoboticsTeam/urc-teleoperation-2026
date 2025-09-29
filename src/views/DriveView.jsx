4cdc6f900d5fd74564debb8d56b4ba43e13b9801
ninaw04 <ninawangcn@gmail.com>
2025-09-29 12:40:49 -0700

    change to file structure

---
+++ src/views/DriveView.jsx
@@ -0,0 +1,31 @@
+import 'react-resizable/css/styles.css' // Import default styles
+import DriveManualInput from '../components/drive/DriveManualInput'
+import CameraPane from '../components/drive/CameraPane'
+import Map from '../components/drive/Map'
+import Controller from '../components/drive/Controller'
+import Wheel from '../components/drive/Wheel'
+
+import '../components/drive/drive.css'
+
+export default function DriveView (){
+    return (
+        <div class = "container">
+            <div class = "item1">
+                <DriveManualInput />
+            </div>
+            <div class = "item2"> 
+                <Map />
+            </div>
+            <div class = "item3"> 
+                <CameraPane />
+                <CameraPane />
+            </div>
+            <div class = "item4">
+                <Controller />
+            </div>
+            <div class = "item5">
+                <Wheel />
+            </div>
+        </div>
+    )
+}
