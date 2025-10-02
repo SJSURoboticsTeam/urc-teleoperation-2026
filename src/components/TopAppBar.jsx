dcd1228cc178577bb583c3eb4d2e21a93800845f
Braden Wagner <bradenwagner06@gmail.com>
2025-10-01 17:17:14 -0700

    More tweaks

--- src/components/TopAppBar.jsx
+++ src/components/TopAppBar.jsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button'
 import MenuIcon from '@mui/icons-material/Menu';
-import NavConnectionStatus from './ConnectionManager';
+import NavConnectionStatus from './BackendConnectionManager';
 import GamepadPanel from './drive/GamepadPanel';
 export default function TopAppBar({ setCurrentView, onVelocitiesChange }) {
   const [drawerOpen, setDrawerOpen] = useState(false);
