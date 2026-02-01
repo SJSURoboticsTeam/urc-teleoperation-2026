83d3ffd2101d37519112ce35e1c777ca68b5dbca
CorV4 <baohuang110406@gmail.com>
2026-01-31 19:55:45 -0800

    implemented a general layout of the science view

--- src/views/ScienceView.jsx
+++ src/views/ScienceView.jsx
@@ -1,16 +1,15 @@
 import 'react-resizable/css/styles.css';
-import CameraPane from '../components/cameras/CameraPane';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 export default function ScienceView () {
-    const variables = ["Stop", "Pause", "Complete"];
+    const variables = ["Pause", "Complete"];
     const [currentStep, setCurrentStep] = useState(0);
     const [cycle, setCycle] = useState(0);
     const steps = ["Drill 10cm in one direction", "Open the door", "Move cup out", "Reverse drill direction", "Dirt moves into cup", "Cup is moved inside"];
     const handleStepClick = () => {
-        setCurrentStep((prev) => (prev + 1) % 7);
-        if (currentStep == 6 && cycle < 3){
+        setCurrentStep((prev) => (prev + 1) % steps.length+1);
+        if (currentStep == steps.length && cycle < 3){
             setCycle((prev)=> (prev+1));
         }
     };
@@ -27,8 +26,29 @@ export default function ScienceView () {
         displayText = currentStep === 0 ? "stopped" : steps[currentStep - 1];
     }
     return (
-        <div className="grid grid-flow-col" style={{ gridTemplateRows: '70%',  gridTemplateColumns: '40% 30% 30%', height: '100%' }}>
-        <div className="h-full flex flex-col justify-center"><CameraPane /> </div>
+        <div className="grid grid-flow-col" >
+        <div className="row-span-3 flex flex-col items-center" >
+            <Box sx = {{
+                border:1, 
+                borderColor: 'black',
+                width: 300,
+                height: 400,
+                display: 'flex',
+                justifyContent: 'center',
+                padding: 1,
+                marginTop:2,
+            }}>
+                details:
+                <br/ >
+                <br/ >
+                Current Step:
+                <br/ >
+                {displayText}
+                <br/ >
+                <br/ >
+                Completed Cycles: {cycle}
+            </Box>
+        </div>        
         <div className="row-span-3 flex flex-col items-center">
             <div className="flex flex-row gap-4 mb-2">
                 <Button variant='contained' onClick={handleStepClick} sx = {{ 
@@ -43,7 +63,7 @@ export default function ScienceView () {
                 }}>
                     Step
                 </Button>
-                <Button variant='contained' onClick={handleStepClick} sx = {{ 
+                <Button variant='contained' onClick={handleStopClick} sx = {{ 
                         border:1,
                         borderColor: 'black',
                         width: 100,
@@ -52,12 +72,12 @@ export default function ScienceView () {
                         marginBottom: 15,
                         marginTop: 5,
                 }}>
-                    Drill
+                    Stop
                 </Button>
             </div>
             <div className="flex flex-col gap-2 mb-2 items-center justify-center">
                 {variables.map((variable) => (
-                    <Button variant='contained' onClick={handleStopClick} sx = {{ 
+                    <Button variant='contained' sx = {{ 
                             border:1,
                             borderColor: 'black',
                             width: 100,
@@ -70,26 +90,6 @@ export default function ScienceView () {
                 </Button>
                ))}
             </div>
-        </div>
-        <div className="row-span-3">
-            details:
-            <Box sx = {{
-                border:1, 
-                borderColor: 'black',
-                width: 300,
-                height: 400,
-                display: 'flex',
-                justifyContent: 'center',
-                padding: 1,
-                marginTop:2,
-            }}>
-                Current Step:
-                <br/ >
-                {displayText}
-                <br/ >
-                <br/ >
-                Completed Cycles: {cycle}
-            </Box>
         </div>
         </div>
     )
