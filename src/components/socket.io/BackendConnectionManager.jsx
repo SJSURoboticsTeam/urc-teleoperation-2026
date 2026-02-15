683bf9d0929808993b06417d817319dd83b5bdea
Braden Wagner <bradenwagner06@gmail.com>
2026-02-14 22:15:40 -0800

    proof-of-concept ui

--- src/components/socket.io/BackendConnectionManager.jsx
+++ src/components/socket.io/BackendConnectionManager.jsx
@@ -9,7 +9,11 @@ import {red} from '@mui/material/colors'
 import { useSocketStatus } from './socket';
 import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
 import SettingsRemoteIcon from '@mui/icons-material/SettingsRemote';
-
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 
 
@@ -20,7 +24,11 @@ export default function NavConnectionStatus({ openPane, setOpenPane }) {
     const [latency, setLatency] = useState(null);
     const [numConnections, setNumConnections] = useState(0);
     const [conntype, setconntype] = useState("Checking...");
-    const [age, setAge] = useState('');
+      const [age, setAge] = useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
   
   function connect() {
@@ -130,22 +138,73 @@ useEffect(() => {
               background: "white",
               border: "1px solid gray",
               padding: "10px",
+              width: "280px"
             }}
           >
     
             
-            <ButtonGroup variant="contained" aria-label="Basic button group">
-                <Button color="error" onClick={ disconnect } variant="contained" sx={{width:120}}>DISCONNECT</Button>
-                <Button color="success" onClick={ connect } variant="contained" sx={{width:120}}>CONNECT</Button>
-            </ButtonGroup>
+            <Box sx={{display: "flex",flexDirection: "row",gap: 1,justifyContent: "center"}}>
+                <Button color="error" onClick={ disconnect } variant="contained" sx={{width:140}}>DISCONNECT</Button>
+                <Button color="success" onClick={ connect } variant="contained" sx={{width:140}}>CONNECT</Button>
+            </Box>
             {isConnected ? (
               <div>
             <Typography  sx={{ color: 'black' }}>Latency: {latency} ms</Typography>
             <Typography  sx={{ color: 'black' }}>Clients Connected: {numConnections}</Typography>
             <Typography  sx={{ color: 'black' }}>Websockets: {conntype}</Typography>
             <hr className="divider" />
-            <Typography  sx={{ color: 'black' }} variant = "h6">DRIVE</Typography>
-
+            <Typography  sx={{ color: 'black' }} variant = "h6">CAN CONNECTIONS</Typography>
+            {/* DRIVE CAN CONNECTION */}
+            <Box sx={{display: "flex",flexDirection: "row",gap: 1}}>
+              <FormControl sx={{flex:1}} size="small">
+                <InputLabel id="demo-simple-select-label">DRIVE</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={age}
+                  label="DRIVE"
+                  onChange={handleChange}
+                  fullwidth
+                >
+                  <MenuItem value={10}>Idle</MenuItem>
+                  <MenuItem value={20}>DRIVE[878]</MenuItem>
+                  <MenuItem value={20}>ARM[878]</MenuItem>
+                  <MenuItem value={30}>DEV_1[89989]</MenuItem>
+                  <MenuItem value={100}>PRI_D[89988]</MenuItem>
+                  <MenuItem value={40}>/dev.usbserial.8998989323</MenuItem>
+                </Select>
+              </FormControl>
+              <Button color="success" onClick={ connect } variant="contained" sx={{width:90}}>CONNECT</Button>
+            </Box>
+            <p></p>
+            
+            {/* ARM CAN CONNECTION */}
+            <Box sx={{display: "flex",flexDirection: "row",gap: 1}}>
+              <FormControl sx={{flex:1}} size="small">
+                <InputLabel id="demo-simple-select-label">ARM</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={age}
+                  label="ARM"
+                  onChange={handleChange}
+                  fullwidth
+                >
+                  <MenuItem value={10}>Idle</MenuItem>
+                  <MenuItem value={20}>DRIVE[878]</MenuItem>
+                  <MenuItem value={50}>ARM[878]</MenuItem>
+                  <MenuItem value={30}>CAN[89988]</MenuItem>
+                  <MenuItem value={40}>/dev.usbserial.8998989323</MenuItem>
+                </Select>
+              </FormControl>
+              <Button color="success" onClick={ connect } variant="contained" sx={{width:90}}>CONNECT</Button>
+            </Box>
+            <hr className="divider" />
+            <Typography  sx={{ color: 'black' }} variant = "h6">FEATURE TOGGLES</Typography>
+            <Box sx={{display: "flex",flexDirection: "col",gap: 1}}>
+              <Button color="success" onClick={ connect } variant="contained">START CPU METRICS</Button>
+              <Button color="success" onClick={ connect } variant="contained">START UNIFI METRICS</Button>
+            </Box>
             </div>
               
               
