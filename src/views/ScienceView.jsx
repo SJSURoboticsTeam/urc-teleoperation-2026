c5d4fa31f59d71f8499233fe605bcb5bd6a7f11d
CorV4 <baohuang110406@gmail.com>
2026-03-21 22:38:54 -0700

    fixed scienceUI layout

--- src/views/ScienceView.jsx
+++ src/views/ScienceView.jsx
@@ -12,6 +12,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import {useRef} from 'react';
 function createData(vials, initialNM, finalNM, delta) {
         return { vials, initialNM, finalNM, delta };
     }
@@ -29,10 +30,10 @@ export default function ScienceView () {
     const exampleFrequency1 = [515, 500, 515, 520, 515, 500, 525, 510, 500, 515, 500];
     const exampleFrequency2 = [545, 540, 545, 550, 540, 555, 545, 540, 545, 535, 545];
     const xTime = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110];
-    const yRange = [0, 100, 200, 300, 400, 500, 600, 700]
+    const containerRef = useRef(null);
     return (
-        <div className="flex flex-1 flex-col justify-center overflow-auto items-center h-full min-h-0" style={{ userSelect: 'none' }}>
-            <div className = 'flex flex-row justify-center items-center'>
+        <div className="flex flex-1 flex-col overflow-auto h-full min-h-0" ref={containerRef} style={{ userSelect: 'none' }}>
+            <div className = 'flex flex-row justify-center'>
                 <Button variant='contained'sx = {{ 
                         border:1,
                         borderColor: 'black',
@@ -68,16 +69,16 @@ export default function ScienceView () {
                         marginBottom: 2,
                         ml: 1,
                 }}>
-                    SCIENCE E-Stop
+                     Science E-Stop
                 </Button>
             </div>
-            <div className="steps">
+            <div className="steps flex justify-center">
                 <div className="step step-accent">Start</div>
                 <div className="step step-accent">Site 1</div>
                 <div className="step step-accent">Site 2</div>
                 <div className="step step-accent">Site 3</div>
             </div>
-            <Box sx={{ minWidth: '100%', minHeight: 600}}>
+            <Box sx={{flex: 1, height: 400}}>
                 <Box sx={{border: 1, borderRadius: 2, borderColor: 'divider'}}>
                     <Tabs
                         value={TabContent}
@@ -92,10 +93,9 @@ export default function ScienceView () {
                 <Box sx={{p:1}}>
                     {tabNum.map((num) => ( TabContent === num ? (
                         <div key={num}>
-                            <div className="flex flex-row gap-4 mb-4">
-                                <Box sx={{width: '50%'}}>
-                                <div className="overflow-x-auto">
-                                    <div className="steps">
+                            <div className="flex flex-row">
+                                <Box sx={{width:'60%', overflowX: 'auto' }}>
+                                    <div className="steps flex">
                                         <div className="step step-accent">Start</div>
                                         <div className="step step-accent">Step 1</div>
                                         <div className="step step-accent">Step 2</div>
@@ -106,26 +106,27 @@ export default function ScienceView () {
                                         <div className="step step-accent">Step 7</div>
                                         <div className="step step-accent">Step 8</div>
                                         <div className="step step-accent">Step 9</div>
+                                        <div className="step step-accent">Step 10</div>
                                     </div>
-                                </div>
                                 </Box>
-                                <Box sx={{ ml: 5, minWidth: 200 }}> Coordinates: (_,_) <br/> Accuracy: ___ <br/> Range: ___ <br/> </Box>
-                                <Button variant="contained"
-                                    sx={{
-                                        border: 1,
-                                        borderColor: "black",
-                                        height: 45,
-                                        width: 100,
-                                        display: "flex",
-                                        justifyContent: "center",
-                                        fontSize: "0.75rem"
-                                    }}
-                                >
-                                    GET GNSS
-                                </Button>
+                                <Box className='flex flex-row' sx={{ ml: 4}}> Coordinates: (_,_) <br/> Accuracy: ___ <br/> Range: ___ <br/>
+                                    <Button variant="contained"
+                                        sx={{
+                                            border: 1,
+                                            borderColor: "black",
+                                            height: 45,
+                                            width: 'auto',
+                                            display: "flex",
+                                            justifyContent: "center",
+                                            ml: 2
+                                        }}
+                                    >
+                                        GET GNSS
+                                    </Button>
+                                </Box>
                             </div>
-                            <div className="flex flex-row gap-4 mb-4">
-                                <Box sx={{ minWidth: '70%', minHeight: 400 }}>
+                            <div className="flex flex-row gap-4 mb-4" style={{ flex: 1, minWidth: 0 }}>
+                                <Box sx={{width:'65%'}}>
                                     <LineChart
                                         series={[
                                         {data: exampleFrequency1, label: 'Frequency 1'},
@@ -133,36 +134,37 @@ export default function ScienceView () {
                                         ]}
                                         xAxis={[{scaleType: 'point', data: xTime, height: 25 }]}
                                         yAxis={[{width: 45 }]}
-
                                     />
                                     </Box>
-                                <TableContainer component={Paper}>
-                                    <Table sx={{ minWidth: '10%', minHeight: 400 }} aria-label="simple table">
-                                        <TableHead>
-                                        <TableRow>
-                                            <TableCell>vials</TableCell>
-                                            <TableCell align="right">initial</TableCell>
-                                            <TableCell align="right">final</TableCell>
-                                            <TableCell align="right">delta</TableCell>
-                                        </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                        {rows.map((row) => (
-                                            <TableRow
-                                            key={row.vials}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                            >
-                                            <TableCell component="th" scope="row">
-                                                {row.vials}
-                                            </TableCell>
-                                            <TableCell align="right">{row.initialNM}</TableCell>
-                                            <TableCell align="right">{row.finalNM}</TableCell>
-                                            <TableCell align="right">{row.delta}</TableCell>
+                                <Box sx={{width: '35%', minHeight: 300}}>
+                                    <TableContainer component={Paper}>
+                                        <Table aria-label="simple table">
+                                            <TableHead>
+                                            <TableRow>
+                                                <TableCell>vials</TableCell>
+                                                <TableCell align="right">initial</TableCell>
+                                                <TableCell align="right">final</TableCell>
+                                                <TableCell align="right">delta</TableCell>
                                             </TableRow>
-                                        ))}
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
+                                            </TableHead>
+                                            <TableBody>
+                                            {rows.map((row) => (
+                                                <TableRow
+                                                key={row.vials}
+                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                <TableCell component="th" scope="row">
+                                                    {row.vials}
+                                                </TableCell>
+                                                <TableCell align="right">{row.initialNM}</TableCell>
+                                                <TableCell align="right">{row.finalNM}</TableCell>
+                                                <TableCell align="right">{row.delta}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                </Box>
                             </div>
                         </div> 
                     ) : null ))}
