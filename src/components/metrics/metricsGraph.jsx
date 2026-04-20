414b8b816b8a91c657020702dc02626856e1c66e
CorV4 <75868215+CorV4@users.noreply.github.com>
2026-04-19 22:49:38 -0700

    Metrics graph (#135)
    * metrics graph wip
    
    * added a tab in extrasView to create a mockup of signal graph
    
    * three metrics graphs using mock data to demo
    
    * added a few more graphs
    
    * deleted frequency and frequency width graph and format improvements
    
    * start and stop buttons
    
    * cleanup panes for extrasview, misc
    
    ---------
    
    Co-authored-by: Braden Wagner <bradenwagner06@gmail.com>

---
+++ src/components/metrics/metricsGraph.jsx
@@ -0,0 +1,241 @@
+import 'react-resizable/css/styles.css';
+import {useAntennaData} from './metrics';
+import { useState, useEffect } from 'react';
+import { LineChart } from '@mui/x-charts/LineChart';
+import {Button, Box} from '@mui/material';
+
+export default function MetricsGraph() {
+    return(
+        <div className="flex flex-col">
+            <div className="flex flex-row">
+                <Box>
+                    <SignalGraph/>
+                </Box>
+                <Box>
+                    <NoiseGraph/>
+                </Box>
+            </div>
+            <div className="flex flex-row">  
+                <Box>
+                    <EfficiencyGraph/>
+                </Box>
+                <Box>
+                    <TxRxGraph/>
+                </Box>
+            </div>
+        </div>
+    )
+}
+
+function SignalGraph() {
+    const antenna = useAntennaData();
+
+    const [running, setRunning] = useState(false);
+    const [time, setTime] = useState([]);
+    const [signalData, setSignalData] = useState([]);
+
+    useEffect(() => {
+        if (!running || antenna.status !== "GOOD" || antenna.roverRSSI == null) return;
+            setSignalData((prev) => {
+                return [...prev, antenna.roverRSSI].slice(-30);
+            });
+            setTime((prev) => {
+                    const updateTime = prev.length === 0 ? 0 : prev.at(-1) + 1;
+                    return [...prev, updateTime].slice(-30);
+            });
+    }, [antenna.status, antenna.roverRSSI, running]);
+
+    return (    
+        <Box sx={{width: '75%'}}>
+            <LineChart
+                height={300}
+                width={500}
+                skipAnimation
+                series={[
+                {   
+                    data:signalData, id: 'Signal Strength', label: 'Signal Strength (dBm)'
+                },]}
+                xAxis={[{ type: 'linear', data: time, label: 'Time (s)' }]}
+                yAxis={[{ label: 'Signal Strength (dBm)', width: 50 }]}
+            />
+        
+            <Button 
+                variant="contained" 
+                onClick={() => setRunning((p) => !p)}
+                sx={{width: 'auto'}}>
+                {running ? 'stop' : 'start'}
+            </Button>
+            <Button
+                variant="outlined"
+                sx={{ ml:1, width: 'auto', fontSize:12}}
+                onClick={() => {
+                setSignalData([]);
+                setTime([]);
+                }}
+            >
+                reset
+            </Button>
+        </Box>
+    );
+}
+
+function NoiseGraph() {
+    const antenna = useAntennaData();
+
+    const [running, setRunning] = useState(false);
+    const [time, setTime] = useState([]);
+    const [noiseData, setNoiseData] = useState([]);
+
+    useEffect(() => {
+        if (!running || antenna.status !== "GOOD" || antenna.noise == null) return;
+            setNoiseData((prev) => {
+                return [...prev, antenna.noise].slice(-30);
+            });
+            setTime((prev) => {
+                    const updateTime = prev.length === 0 ? 0 : prev.at(-1) + 1;
+                    return [...prev, updateTime].slice(-30);
+            });
+    }, [antenna.status, antenna.noise, running]);
+
+    return (    
+        <Box sx={{width: '75%'}}>
+            <LineChart
+                height={300}
+                width={500}
+                skipAnimation
+                series={[
+                {   
+                    data:noiseData, id: 'Noise', label: 'Noise (dBm)'
+                },]}
+                xAxis={[{ type: 'linear', data: time, label: 'Time (s)' }]}
+                yAxis={[{ label: 'Noise (dBm)', width: 50 }]}
+            />
+
+            <Button 
+                variant="contained" 
+                onClick={() => setRunning((p) => !p)}
+                sx={{width: 'auto'}}>
+                {running ? 'stop' : 'start'}
+            </Button>
+            <Button
+                variant="outlined"
+                sx={{ ml:1, width: 'auto', fontSize:12}}
+                onClick={() => {
+                setNoiseData([]);
+                setTime([]);
+                }}
+            >
+                reset
+            </Button>
+        </Box>
+    );
+}
+
+function EfficiencyGraph() {
+    const antenna = useAntennaData();
+
+    const [time, setTime] = useState([]);
+    const [running, setRunning] = useState(false);
+    const [efficiencyData, setEfficiencyData] = useState([]);
+
+    useEffect(() => {
+        if (!running || antenna.status !== "GOOD" || antenna.efficiency == null) return;
+            setEfficiencyData((prev) => {
+                return [...prev, antenna.efficiency].slice(-30);
+            });
+            setTime((prev) => {
+                    const updateTime = prev.length === 0 ? 0 : prev.at(-1) + 1;
+                    return [...prev, updateTime].slice(-30);
+            });
+    }, [antenna.status, antenna.efficiency, running]);
+
+    return (    
+        <Box sx={{width: '75%'}}>
+            <LineChart
+                height={300}
+                width={500}
+                skipAnimation
+                series={[
+                {   
+                    data:efficiencyData, id: 'Efficiency', label: 'Efficiency (%)'
+                },]}
+                xAxis={[{ type: 'linear', data: time, label: 'Time (s)' }]}
+                yAxis={[{ label: 'Efficiency (%)', width: 50 }]}
+            />
+        
+            <Button
+                variant="contained" 
+                onClick={() => setRunning((p) => !p)}
+                sx={{width: 'auto'}}>
+                {running ? 'stop' : 'start'}
+            </Button>
+            <Button
+                variant="outlined"
+                sx={{ ml:1, width: 'auto', fontSize:12}}
+                onClick={() => {
+                setEfficiencyData([]);
+                setTime([]);
+                }}
+            >
+                reset
+            </Button>
+        </Box>
+    );
+}
+
+function TxRxGraph() {
+    const antenna = useAntennaData();
+
+    const [time, setTime] = useState([]);
+    const [TxData, setTxData] = useState([]);
+    const [RxData, setRxData] = useState([]);
+    const [running, setRunning] = useState(false);
+
+    useEffect(() => {
+        if (!running || antenna.status !== "GOOD" || antenna.txrate == null || antenna.rxrate == null) return;
+            setTxData((prev) => {
+                return [...prev, antenna.txrate].slice(-30);
+            });
+            setRxData((prev) => {
+                return [...prev, antenna.rxrate].slice(-30);
+            });
+            setTime((prev) => {
+                    const updateTime = prev.length === 0 ? 0 : prev.at(-1) + 1;
+                    return [...prev, updateTime].slice(-30);
+            });
+    }, [antenna.status, antenna.txrate, antenna.rxrate, running]);
+
+    return (    
+        <Box sx={{width: '75%'}}>
+            <LineChart
+                height={300}
+                width={500}
+                skipAnimation
+                series={[
+                { data:TxData, id: 'Tx', label: 'Tx (Mbps)'},
+                { data:RxData, id: 'Rx', label: 'Rx (Mbps)'},
+                ]}
+                xAxis={[{ type: 'linear', data: time, label: 'Time (s)' }]}
+                yAxis={[{ label: 'Tx/Rx (Mbps)', width: 50 }]}
+            />
+        
+            <Button
+                variant="contained" 
+                onClick={() => setRunning((p) => !p)}
+                sx={{width: 'auto'}}>
+                {running ? 'stop' : 'start'}
+            </Button>
+            <Button
+                variant="outlined"
+                sx={{ ml:1, width: 'auto', fontSize:12}}
+                onClick={() => {
+                setTxData([]);
+                setRxData([]);
+                setTime([]);
+                }}
+            >
+                reset
+            </Button>
+        </Box>
+    );
+}
