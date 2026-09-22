f37a80f8ae2871be927c286a596de492701c17ee
Anh Luong <luonganh0128@gmail.com>
2026-09-21 19:08:58 -0700

    add a tooltip for no data

--- src/components/battery/batteryDisplay.jsx
+++ src/components/battery/batteryDisplay.jsx
@@ -1,6 +1,6 @@
 import { robotsocket } from "../socket.io/socket";
 import { useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Tooltip, Typography } from "@mui/material";
 
 const MAX_BATTERY_VOLTAGE = 40;
 const MIN_BATTERY_VOLTAGE = 36;
@@ -27,62 +27,65 @@ export default function BatteryDisplay() {
 
     const hasData = batteryPercentage !== null;
     const isLow = hasData && batteryPercentage < 20;
+    const toolTipTitle = hasData ? "" : "No data";
 
     return (
-         <Box sx={{ display: "flex", alignItems: "center" }}>
-            {/* battery body */}
-            <Box
-                sx={{
-                    position: "relative",
-                    isolation: 'isolate',
-                    width: 34,
-                    height: 18,
-                    borderRadius: "5px",
-                    overflow: "hidden",
-                    bgcolor: "action.disabledBackground",
-                }}
-            >
-                {/* fill */}
+        <Tooltip title={toolTipTitle} arrow>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+                {/* battery body */}
                 <Box
                     sx={{
-                        position: "absolute",
-                        top: 0,
-                        bottom: 0,
-                        left: 0,
-                        width: `${batteryPercentage}%`,
-                        bgcolor: isLow ? "red" : "white",
-                        transition: "width 0.4s ease",
-                    }}
-                />
-                {/* number */}
-                <Typography
-                    component="span"
-                    sx={{
-                        position: "absolute",
-                        inset: 0,
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        fontSize: 12,
-                        fontWeight: 700,
-                        fontVariantNumeric: "tabular-nums",
-                        color: "white",
-                        mixBlendMode: isLow ? "normal" : "difference",
+                        position: "relative",
+                        isolation: 'isolate',
+                        width: 34,
+                        height: 18,
+                        borderRadius: "5px",
+                        overflow: "hidden",
+                        bgcolor: "action.disabledBackground",
                     }}
                 >
-                    {hasData ? Math.round(batteryPercentage) : "?"}
-                </Typography>
+                    {/* fill */}
+                    <Box
+                        sx={{
+                            position: "absolute",
+                            top: 0,
+                            bottom: 0,
+                            left: 0,
+                            width: `${batteryPercentage}%`,
+                            bgcolor: isLow ? "red" : "white",
+                            transition: "width 0.4s ease",
+                        }}
+                    />
+                    {/* number */}
+                    <Typography
+                        component="span"
+                        sx={{
+                            position: "absolute",
+                            inset: 0,
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            fontSize: 12,
+                            fontWeight: 700,
+                            fontVariantNumeric: "tabular-nums",
+                            color: "white",
+                            mixBlendMode: isLow ? "normal" : "difference",
+                        }}
+                    >
+                        {hasData ? Math.round(batteryPercentage) : "?"}
+                    </Typography>
+                </Box>
+                {/* terminal nub */}
+                <Box
+                    sx={{
+                        width: 2,
+                        height: 6,
+                        ml: "1px",
+                        bgcolor: "action.disabled",
+                        borderRadius: "0 2px 2px 0",
+                    }}
+                />
             </Box>
-            {/* terminal nub */}
-            <Box
-                sx={{
-                    width: 2,
-                    height: 6,
-                    ml: "1px",
-                    bgcolor: "action.disabled",
-                    borderRadius: "0 2px 2px 0",
-                }}
-            />
-        </Box>
+        </Tooltip>
     );
 }
