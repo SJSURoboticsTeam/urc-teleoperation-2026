f4465f2bf25f8379bf673f02d96602e2c2a773c8
Anh Luong <luonganh0128@gmail.com>
2026-09-24 19:06:01 -0700

    move processing from frontend to backend

--- server/battery.py
+++ server/battery.py
@@ -1,15 +1,19 @@
 import random
 import asyncio
 
+MAX_VOLTAGE = 40
+MIN_VOLTAGE = 36
+
 async def get_battery_voltage(sio):
     # TODO: implement once firmware is ready
     pass
 
 async def send_fake_battery_voltage(sio):
-    voltage = 40
+    voltage = MAX_VOLTAGE
     while True:
-        voltage -= .02
-        if voltage < 36:
-            voltage = 40
-        await sio.emit('batteryVoltage', voltage + random.gauss(0, .05))
+        voltage -= .02 + random.gauss(0, .05)
+        if voltage < MIN_VOLTAGE:
+            voltage = MAX_VOLTAGE
+        percentage = ((voltage - MIN_VOLTAGE) / (MAX_VOLTAGE - MIN_VOLTAGE)) * 100
+        await sio.emit('batteryPercentage', min(100, max(0, percentage)))
         await asyncio.sleep(.25)
--- src/components/battery/batteryDisplay.jsx
+++ src/components/battery/batteryDisplay.jsx
@@ -2,25 +2,18 @@ import { robotsocket } from "../socket.io/socket";
 import { useEffect, useState } from "react";
 import { Box, Tooltip, Typography } from "@mui/material";
 
-const MAX_BATTERY_VOLTAGE = 40;
-const MIN_BATTERY_VOLTAGE = 36;
-
 export default function BatteryDisplay() {
 	const [batteryPercentage, setBatteryPercentage] = useState(null);
 
 	useEffect(() => {
-		const handler = (voltage) => {
-			let percentage = ((voltage - MIN_BATTERY_VOLTAGE) / (MAX_BATTERY_VOLTAGE - MIN_BATTERY_VOLTAGE)) * 100;
-			setBatteryPercentage(Math.max(0, Math.min(100, percentage)));
-		}
-		const handleDisconnection = () => {
-			setBatteryPercentage(null);
-		};
+		const handler = (percentage) =>	setBatteryPercentage(percentage);
+		const handleDisconnection = () => setBatteryPercentage(null);
 
-		robotsocket.on("batteryVoltage", handler);
+		robotsocket.on("batteryPercentage", handler);
 		robotsocket.on("disconnect", handleDisconnection);
+
 		return () => {
-			robotsocket.off("batteryVoltage", handler);
+			robotsocket.off("batteryPercentage", handler);
 			robotsocket.off("disconnect", handleDisconnection);
 		}
 	}, []);
